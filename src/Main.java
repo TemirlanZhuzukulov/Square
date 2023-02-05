c32bdb7cc7f27de78f32476a32c6eb652f82dc53
Notnik_kg <j.temirlan141002@gmail.com>
2023-02-05 14:45:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Square square = new Square();
+        square.setLength(5);
+        square.setWidth(5);
+        square.square(square.getLength(), square.getWidth());
+
+
+    }
+}
